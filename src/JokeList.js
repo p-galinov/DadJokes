6f47087b2bb31c5c62605ea9584a31268a4dacd5
Plamen <pgalinov95@gmail.com>
2020-11-30 21:56:11 +0200

    added responsive design

--- src/JokeList.js
+++ src/JokeList.js
@@ -72,7 +72,6 @@ class JokeList extends Component {
         </div>
       );
     }
-    //let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
     return (
       <div className='JokeList'>
         <div className='JokeList-sidebar'>
